fb042db92b6abb20f5cf6b512c1cbcc866eafd8e
Garrett Thompson <gtcruz.gt@gmail.com>
2020-05-17 00:25:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ function greet(timeString) {
 
 /* Write your implementation of displayMessage() */
 function displayMessage(oneString) {
-  const newHeader = document.createElement("h1");
+  let newHeader = document.createElement("h1");
   newHeader = greet(timeString);
 }
