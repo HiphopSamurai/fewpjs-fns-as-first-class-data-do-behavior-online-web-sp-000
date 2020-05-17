bd1873abcef310673f1e1de0c382b6d75bd6fe80
Garrett Thompson <gtcruz.gt@gmail.com>
2020-05-17 00:13:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ function greet(timeString) {
   const time = timsString.split(' ');
   if (time < 12) {
     return 'Good Morning';
-    else if (time > 12 && time < 17 ) {
+  }else if (time > 12 && time < 17 ) {
       return 'Good Afternoon';
+    }else {
+      return 'Good Evening'
     }
-  }
+  
 
 
 }
