8407986f0c34c3f7ad8cb01aabb00e2b59d5b661
Garrett Thompson <gtcruz.gt@gmail.com>
2020-05-17 00:14:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 function greet(timeString) {
   const time = timsString.split(' ');
-  if (time < 12) {
+  if (parseInt(time) < 12) {
     return 'Good Morning';
   }else if (time > 12 && time < 17 ) {
       return 'Good Afternoon';
